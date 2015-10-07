231a0960e4c163a0df22130d0c72b662cbafc992
Alex Cusack <cusackalex@gmail.com>
2015-10-06 17:04:57 -0700

    check if assigned date is a weekend. if weekend, reschedule

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import * as redux from 'redux'
 import uuid from 'uuid'
 
 const dispatch = (state = {}, action) => {
-  console.log(action)
-  if (action.type === 'LOAD_SEED_NAMES'){
+  if (action.type === 'LOAD_SEED_NAMES') {
     console.log(loadSeedNameFacts(action.names, action.startDate))
+
   }
   /* build state */
   return state
@@ -18,11 +18,12 @@ const loadSeedNameFacts = (names, date) => {
   const uniqueNames = new Set(names)
   uniqueNames.forEach((name) => {
     const entityID = uuid()
-    facts.push(['assert', entityID, 'name', name])
+    facts.push(['assert', entityID, 'User/name', name])
     mapIdToName[name] = entityID
   })
   names.forEach((name) => {
     const assignmentID = uuid()
+    while (dateIsWeekend(currentDate)) { currentDate.setDate(currentDate.getDate() + 1) }
     facts.push(['assert', assignmentID, 'assignment/Date', currentDate])
     facts.push(['assert', assignmentID, 'assignment/User', mapIdToName[name]])
     currentDate.setDate(currentDate.getDate() + 1)
@@ -31,19 +32,19 @@ const loadSeedNameFacts = (names, date) => {
   return facts
 }
 
-
 const getDate = (dateString) => { return new Date(Date.parse(dateString)) }
+const dateIsWeekend = (dateString) => { return dateString.getDay() === 6 || dateString.getDay() === 0 }
 
 const store = redux.createStore(dispatch)
 
 store.dispatch({
   type: 'LOAD_SEED_NAMES', // THE COMMAND
   names: ['Sherry', 'Boris', 'Vicente', 'Matte', 'Jack', 'Sherry',
-   'Matte', 'Kevin', 'Kevin', 'Vicente', 'Zoe', 'Kevin',
-   'Matte', 'Zoe', 'Jay', 'Boris', 'Eadon', 'Sherry',
-   'Franky', 'Sherry', 'Matte', 'Franky', 'Franky', 'Kevin',
-   'Boris', 'Franky', 'Vicente', 'Luis', 'Eadon', 'Boris',
-   'Kevin', 'Matte', 'Jay', 'James', 'Kevin', 'Sherry',
+   // 'Matte', 'Kevin', 'Kevin', 'Vicente', 'Zoe', 'Kevin',
+   // 'Matte', 'Zoe', 'Jay', 'Boris', 'Eadon', 'Sherry',
+   // 'Franky', 'Sherry', 'Matte', 'Franky', 'Franky', 'Kevin',
+   // 'Boris', 'Franky', 'Vicente', 'Luis', 'Eadon', 'Boris',
+   // 'Kevin', 'Matte', 'Jay', 'James', 'Kevin', 'Sherry',
    'Sherry', 'Jack', 'Sherry', 'Jack'],
   startDate: getDate('2015-10-16'),
 })
@@ -59,5 +60,5 @@ const generateScheduleForCurrentMonth = (applicationState) => {
   // ]
 }
 
-const currentSchedule = scheduleForCurrentMonth(store.getState())
-console.log(currentSchedule)
+// const currentSchedule = scheduleForCurrentMonth(store.getState())
+// console.log(currentSchedule)
